2d7d08b5499e1a2ceb8ca003b163ce22a35f45dd
Johnathan Barker <johnathanfbarker@hotmail.com>
2024-05-06 15:26:25 +0100

    Task 4

---
+++ addstudents.php
@@ -0,0 +1,83 @@
+<?php
+include("_includes/config.inc");
+include("_includes/dbconnect.inc");
+include("_includes/functions.inc");
+
+// Display the form initially
+function displayForm() {
+    echo <<<EOD
+    <h2>Add New Student</h2>
+    <form name="frmdetails" action="" method="post">
+        Student ID:
+        <input name="txtstudentid" type="text" value="" /><br/>
+        First Name:
+        <input name="txtfirstname" type="text" value="" /><br/>
+        Last Name:
+        <input name="txtlastname" type="text" value="" /><br/>
+        Date of Birth:
+        <input name="dob" type="date" /><br/> <!-- Add this line for date input -->
+        Number and Street:
+        <input name="txthouse" type="text" value="" /><br/>
+        Town:
+        <input name="txttown" type="text" value="" /><br/>
+        County:
+        <input name="txtcounty" type="text" value="" /><br/>
+        Country:
+        <input name="txtcountry" type="text" value="" /><br/>
+        Postcode:
+        <input name="txtpostcode" type="text" value="" /><br/>
+        Password:
+        <input name="txtpassword" type="password" value="" /><br/>
+        <input type="submit" value="Save" name="submit"/>
+    </form>
+    EOD;
+}
+
+// Check if the user is logged in
+if (isset($_SESSION['id'])) {
+    echo template("templates/partials/header.php");
+    echo template("templates/partials/nav.php");
+
+    // If the form has been submitted
+    if (isset($_POST['submit'])) {
+        // Validate and sanitize input data
+        $studentId = mysqli_real_escape_string($conn, $_POST['txtstudentid']);
+        $firstName = mysqli_real_escape_string($conn, $_POST['txtfirstname']);
+        $lastName = mysqli_real_escape_string($conn, $_POST['txtlastname']);
+        $house = mysqli_real_escape_string($conn, $_POST['txthouse']);
+        $town = mysqli_real_escape_string($conn, $_POST['txttown']);
+        $county = mysqli_real_escape_string($conn, $_POST['txtcounty']);
+        $country = mysqli_real_escape_string($conn, $_POST['txtcountry']);
+        $postcode = mysqli_real_escape_string($conn, $_POST['txtpostcode']);
+        $dob = mysqli_real_escape_string($conn, $_POST['dob']);
+        // Hash the password securely (using bcrypt)
+        $password = password_hash($_POST['txtpassword'], PASSWORD_BCRYPT);
+
+        // Check if the student ID already exists
+        $existingStudentQuery = "SELECT studentid FROM student WHERE studentid = '$studentId'";
+        $existingStudentResult = mysqli_query($conn, $existingStudentQuery);
+        if (mysqli_num_rows($existingStudentResult) > 0) {
+            // Duplicate student ID found, prompt user to try again
+            echo "<p>Duplicate student ID. Please try again.</p>";
+            displayForm();
+        } else {
+            // Insert the student record into the database
+            $sql = "INSERT INTO student (studentid, password, firstname, lastname, house, town, county, country, postcode, dob) 
+                    VALUES ('$studentId', '$password', '$firstName', '$lastName', '$house', '$town', '$county', '$country', '$postcode', '$dob')";
+            $result = mysqli_query($conn, $sql);
+            echo "<p>Your student record has been added</p>";
+            header("Location: students.php");
+        }
+    } else {
+        displayForm();
+    }
+
+    // Render the template
+    echo template("templates/default.php", $data);
+} else {
+    // Redirect to the login page
+    header("Location: index.php");
+    exit; // Ensure that no further code execution occurs
+}
+echo template("templates/partials/footer.php");
+?>
--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -3,6 +3,7 @@
 
    <a href="modules.php">My Modules</a> |
    <a href="students.php">All Students</a> |
+   <a href="addstudents.php">Add Students</a> |
    <a href="assignmodule.php">Assign Module</a> |
    <a href="details.php">My Details</a> |
    <a href="logout.php">Logout</a>
